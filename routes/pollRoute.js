a400e3b8b657f7184fba0d837b0227d1ebb6351c
pknelakuditi <pavannelakuditi@gmail.com>
2015-08-30 16:55:52 -0400

    removed idvalidator

--- routes/pollRoute.js
+++ routes/pollRoute.js
@@ -3,7 +3,6 @@
 var express = require('express');
 var router = express.Router();
 var pollValidator = require('../helper/pollValidator');
-//var idValidator=require('../helper/idValidator');
 var MongoClient = require('mongodb').MongoClient;
 var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/polls';
 var Poll = require('../model/Poll').init(mongoURI, MongoClient);
