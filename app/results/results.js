dbd03be996679020027fbf3cd2dd999a30f1787d
Han Koh <rhgksrua@gmail.com>
2015-08-31 17:52:44 -0500

    added graph

--- app/results/results.js
+++ app/results/results.js
@@ -19,23 +19,24 @@
             rs.getUrl = function(url) {
                 rs.url = url;
             };
-            rs.getPollResult = function(){
+            rs.getPollResult = function() {
                 $http.post('/poll/' + rs.url + '/results')
                     .then(function(response) {
                        // console.log(response.data);
                         rs.poll = response.data;
                         console.log(rs.poll);
+                        rs.poll.total = totalVotes(rs.poll);
                         // this callback will be called asynchronously
                         // when the response is available
                     }, function(response) {
                         var temp = {
                             question: 'fav animal',
                             choices: [
-                                {id: 'choice0', name: 'cat', vote: 0},
-                                {id: 'choice1', name: 'dog', vote: 0},
-                                {id: 'choice2', name: 'cow', vote: 0},
-                                {id: 'choice3', name: 'horse', vote: 0},
-                                {id: 'choice4', name: 'bird', vote: 0},
+                                {id: 'choice0', name: 'cat', vote: 4},
+                                {id: 'choice1', name: 'dog', vote: 5},
+                                {id: 'choice2', name: 'cow', vote: 2},
+                                {id: 'choice3', name: 'horse', vote: 9},
+                                {id: 'choice4', name: 'bird', vote: 3},
                             ]
                         };
                         console.log('ajax error');
@@ -43,7 +44,13 @@
                         // or server returns response with an error status.
                         rs.poll.question = temp.question;
                         rs.poll.choices = temp.choices;
+                        rs.poll.total = rs.totalVotes(rs.poll);
                     });
+            };
+            rs.totalVotes = function(data) {
+                return data.choices.reduce(function(pv, cv) {
+                    return pv + cv.vote;
+                }, 0);
             };
         }])
         .controller('resultCtrl', ["$location", "resultService" ,function($location, resultService){
