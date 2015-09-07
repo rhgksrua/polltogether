37bb2fa38b02ca517720fad3f9e785c6a3c8b1eb
Han Koh <rhgksrua@gmail.com>
2015-09-07 00:29:36 -0500

    refactored vote submits

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -26,39 +26,6 @@ angular.module('pollApp.pollVote', ['ngRoute'])
             poll.id = id;
         };
 
-        // container for observer
-        var observerCallbacks = {};
-
-        /**
-         * registerCallback
-         * registers callback function for observers
-         *
-         * @param {string} name of group to notify
-         * @param {function} callback
-         * @return {undefined}
-         */
-        poll.registerCallback = function(name, callback) {
-            if (observerCallbacks[name]) {
-                observerCallbacks[name].push(callback);
-            } else {
-                observerCallbacks[name] = [];
-                observerCallbacks[name].push(callback);
-            }
-        };
-
-        /**
-         * notifyObservers
-         * executes callback where needed.
-         * 
-         * @param  {string} name
-         * @return {undefined}
-         */
-        var notifyObservers = function(name) {
-            angular.forEach(observerCallbacks[name], function(callback) {
-                callback();
-            });
-        };
-
         /**
          * getPoll
          * ajax request to server api to get poll
@@ -81,35 +48,25 @@ angular.module('pollApp.pollVote', ['ngRoute'])
          */
         poll.submitVote = function(vote) {
             console.log(vote);
-            $http.post('/poll/vote/submit', vote)
+            return $http.post('/poll/vote/submit', vote)
                 .then(function(response) {
-                    if (response.data.error) {
-                        // server error
-                        console.log(response.data);
-                        poll.failed = true;
-                        poll.status = 'Vote Failed';
-                        notifyObservers('submitVote');
-                        return;
-                    }
-                    console.log('vote submit ajax successful');
-                    poll.submitted = true;
-                    poll.failed = false;
-                    poll.status = 'Vote Submitted!';
-                    notifyObservers('submitVote');
-                }, function(response) {
-                    // ajax fail
-                    console.log('vote submit ajax error');
-                    poll.failed = true;
-                    poll.status = 'Vote Failed!';
-                    notifyObservers('submitVote');
+                    return response;
                 });
         };
     }])
     .controller('pollVoteCtrl', ['voteService', '$routeParams' ,"$location", function(voteService, $routeParams,$location) {
         var vc = this;
+
+        // holds user choice
         vc.choice = undefined;
+        
+        // url or id
         vc.id = $routeParams.id;
+
+        // status mesasge on modal
         vc.status = 'Submitting...';
+
+        // vote success/fail status
         vc.failed = false;
 
         // Set url for ajax request
@@ -119,40 +76,11 @@ angular.module('pollApp.pollVote', ['ngRoute'])
         voteService.getPoll().then(function(response) {
             console.log('ajax success');
             vc.poll = response.data;
+            vc.getError = false;
         }).then(null, function(response) {
             console.log('something wrong', response);
+            vc.getError = true;
         });
-        
-        /**
-         * updatePolls
-         * Observer callback function to update ajax response from service instance
-         *
-         * @return {undefined}
-         */
-        var updatePolls = function() {
-            vc.poll = voteService.poll;
-            vc.getError = voteService.getError;
-        };
-
-        /**
-         * submitStatus
-         * Update ajax status based on reponse
-         *
-         * @return {undefined}
-         */
-        var submitStatus = function() {
-            console.log('setting new status');
-            vc.submitted = voteService.submitted;
-            vc.status = voteService.status;
-            vc.failed = voteService.failed;
-            if (vc.failed) {
-                vc.error = 'Cannot submit vote at this time';
-            }
-        };
-
-        // adding callback function to listen for ajax response object.
-        voteService.registerCallback('getPoll', updatePolls);
-        voteService.registerCallback('submitVote', submitStatus);
         
         /**
          * toggleChoice
@@ -178,7 +106,16 @@ angular.module('pollApp.pollVote', ['ngRoute'])
                 vc.failed = true;
                 vc.status = 'Need to pick one';
             } else if (vc.choice !== undefined) {
-                voteService.submitVote({id: vc.id, choice: vc.choice});
+                voteService.submitVote({id: vc.id, choice: vc.choice})
+                    .then(function(response) {
+                        // success
+                        vc.status = 'Vote submitted!';
+                        vc.failed = false;
+                        vc.submitted = true;
+                    }).then(null, function(response) {
+                        // failed
+                        vc.failed = true;
+                    });
             }
         };
 
