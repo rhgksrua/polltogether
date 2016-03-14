0e4841133dff00c52f0b85c497b31b1654276e5b
Han Koh <rhgksrua@gmail.com>
2016-03-14 14:20:43 -0500

    added chartjs

--- app/main.js
+++ app/main.js
@@ -650,7 +650,7 @@ angular.module('pollApp.userPassword')
 (function() {
     'use strict';
 
-    angular.module('pollApp.pollResult', ['ngRoute','ngAnimate','ngMessages'])
+    angular.module('pollApp.pollResult', ['ngRoute','ngAnimate','ngMessages', 'chart.js'])
 
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider.when('/vote/:id/results', {
@@ -688,7 +688,7 @@ angular.module('pollApp.userPassword')
              */
             rs.getPollResult = function() {
                 rs.errors.exists = true;
-                $http.get('/poll/' + rs.url + '/results')
+                return $http.get('/poll/' + rs.url + '/results')
                     .then(function(response) {
                         if (response.data.error) {
                             rs.errors.exists = false;
@@ -696,10 +696,10 @@ angular.module('pollApp.userPassword')
                         }
                         rs.poll.question = response.data.question;
                         rs.poll.choices = response.data.choices;
-                        console.log(rs.poll);
+                        //console.log('choices', response.data.choices);
                         rs.poll.total = rs.totalVotes(rs.poll);
-                    }, function(response) {
-                        console.log('ajax error');
+                        //console.log('poll', rs.poll);
+                        return response;
                     });
             };
 
@@ -718,10 +718,44 @@ angular.module('pollApp.userPassword')
         }])
         .controller('resultCtrl', ["$location", "resultService", "$routeParams" ,function($location, resultService, $routeParams){
             var rc = this;
+            var chart;
+
+            /**
+             * toChart
+             *
+             * @param chart
+             * @returns {undefined}
+             */
+            var toChart = function(choices) {
+                var labels = [];
+                var data = [];
+                choices.forEach(function(el) {
+                    console.log('el', el);
+                    labels.push(el.name);
+                    data.push(el.vote);
+                });
+                return {
+                    labels: labels,
+                    data: data,
+                    series: []
+                };
+            };
+
             rc.errors = resultService.errors;
             resultService.getUrl($routeParams.id);
             rc.poll = resultService.poll;
-            resultService.getPollResult();
+
+            resultService.getPollResult()
+                .then(function(response) {
+                    console.log(response.data);
+                    return response;
+                })
+                .then(function(response) {
+                    rc.chart = toChart(rc.poll.choices);
+                    console.log('chart', rc.chart);
+                    return response;
+                });
+
         }]);
 })();
 
--- app/results/results.js
+++ app/results/results.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('pollApp.pollResult', ['ngRoute','ngAnimate','ngMessages'])
+    angular.module('pollApp.pollResult', ['ngRoute','ngAnimate','ngMessages', 'chart.js'])
 
         .config(['$routeProvider', function($routeProvider) {
             $routeProvider.when('/vote/:id/results', {
@@ -39,7 +39,7 @@
              */
             rs.getPollResult = function() {
                 rs.errors.exists = true;
-                $http.get('/poll/' + rs.url + '/results')
+                return $http.get('/poll/' + rs.url + '/results')
                     .then(function(response) {
                         if (response.data.error) {
                             rs.errors.exists = false;
@@ -47,10 +47,10 @@
                         }
                         rs.poll.question = response.data.question;
                         rs.poll.choices = response.data.choices;
-                        console.log(rs.poll);
+                        //console.log('choices', response.data.choices);
                         rs.poll.total = rs.totalVotes(rs.poll);
-                    }, function(response) {
-                        console.log('ajax error');
+                        //console.log('poll', rs.poll);
+                        return response;
                     });
             };
 
@@ -69,10 +69,44 @@
         }])
         .controller('resultCtrl', ["$location", "resultService", "$routeParams" ,function($location, resultService, $routeParams){
             var rc = this;
+            var chart;
+
+            /**
+             * toChart
+             *
+             * @param chart
+             * @returns {undefined}
+             */
+            var toChart = function(choices) {
+                var labels = [];
+                var data = [];
+                choices.forEach(function(el) {
+                    console.log('el', el);
+                    labels.push(el.name);
+                    data.push(el.vote);
+                });
+                return {
+                    labels: labels,
+                    data: data,
+                    series: []
+                };
+            };
+
             rc.errors = resultService.errors;
             resultService.getUrl($routeParams.id);
             rc.poll = resultService.poll;
-            resultService.getPollResult();
+
+            resultService.getPollResult()
+                .then(function(response) {
+                    console.log(response.data);
+                    return response;
+                })
+                .then(function(response) {
+                    rc.chart = toChart(rc.poll.choices);
+                    console.log('chart', rc.chart);
+                    return response;
+                });
+
         }]);
 })();
 
