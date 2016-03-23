880003d5616bab59f8a4e1dc26c12dc9ed00f511
Han Koh <rhgksrua@gmail.com>
2016-03-22 22:02:59 -0500

    spec test bug fix

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config){
             'app/bower_components/angular-bootstrap/ui-bootstrap.js',
             'app/create/*.js',
             'app/create/modal/*.js',
-            'app/create/services/*.js'
+            'app/create/services/*.js',
         ],
 
         autoWatch : true,
